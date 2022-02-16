c3e96107515ba353f45bfea10ee8028dce26bfc1
abdullahaslam306 <f178135@nu.edu.pk>
2022-02-16 16:52:45 +0500

    boiler plate updated

---
+++ src/configs/env.js
@@ -0,0 +1,9 @@
+module.exports = {
+    dburi : 'mongodb+srv://matrix:matrix123@cluster0.h2dzm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    SESSION_OUT : 30 * 86400 * 1000,
+    SESSION_NAME: 'sid',
+    SESSION_SECRET: 'secret',  
+    environment : 'DEV',
+    EMAIL_USER:'abc',
+    EMAIL_PASS:'123'
+}
